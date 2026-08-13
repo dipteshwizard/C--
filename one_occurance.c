7efb93b858554d76e6243401a9fe14c853c92b71
dipteshwizard <dipteshmandal1@gmail.com>
2026-08-13 15:20:11 +0530

    Elements with exactly one occurance in an array

---
+++ one_occurance.c
@@ -0,0 +1,32 @@
+/*Find the elements with exactly one occurrence in an array*/
+#include <stdio.h>
+
+int main() {
+    int arr[10], i, j, n, count = 0;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("The elements with exactly one occurrence are: ");
+
+    for (i = 0; i < n; i++) {
+        count = 0;
+        for (j = 0; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+        if (count == 1) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
