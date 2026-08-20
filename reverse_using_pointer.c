2a817653e58ef79acb48ac51d407c17f2bf16345
dipteshwizard <dipteshmandal1@gmail.com>
2026-08-20 14:57:47 +0530

    Reverse of an array in place using pointers

---
+++ reverse_using_pointer.c
@@ -0,0 +1,33 @@
+/*Reverse an array in place using pointers*/
+#include <stdio.h>
+void reverseArray(int *arr, int size) {
+    int *start = arr;
+    int *end = arr + size - 1;
+    while (start < end) {
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+}
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("Original array elements:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    reverseArray(arr, size);
+    
+    printf("Array elements after reversing:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
